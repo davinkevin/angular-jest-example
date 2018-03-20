3e194ef78ae3db111eb3a9234947730b9757278c
davinkevin <davin.kevin@gmail.com>
2018-03-20 06:03:59 +0100

    chore(init): setup the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my wonderful app with a very long name which should be truncated';
 }
